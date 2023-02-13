68dbca10fed15c4346ab4f5225fd2fa92135f554
ArthurOliveira01 <arthurods37@gmail.com>
2023-02-13 15:16:39 -0400

    third post

--- src/Posts.js
+++ src/Posts.js
@@ -72,6 +72,41 @@ export default function Posts(){
         </div>
       </div>
     </div>
+    <div class="post">
+      <div class="topo">
+        <div class="usuario">
+          <img src="assets/barked.svg" alt="barked"/>
+          barked
+        </div>
+        <div class="acoes">
+          <ion-icon name="ellipsis-horizontal"></ion-icon>
+        </div>
+      </div>
+
+      <div class="conteudo">
+        <img src="assets/dog.svg" alt="dog" />
+      </div>
+
+      <div class="fundo">
+        <div class="acoes">
+          <div onclick="like()">
+            <ion-icon name="heart-outline"></ion-icon>
+            <ion-icon name="chatbubble-outline"></ion-icon>
+            <ion-icon name="paper-plane-outline"></ion-icon>
+          </div>
+          <div>
+            <ion-icon name="bookmark-outline"></ion-icon>
+          </div>
+        </div>
+
+        <div class="curtidas">
+          <img src="assets/adorable_animals.svg" alt="adorable_animals"/>
+          <div class="texto">
+            Curtido por <strong>adorable_animals</strong> e <strong>outras 99.159 pessoas</strong>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
     );
 }
