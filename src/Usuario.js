1d4b801a899b2557cd90f3f53c368f8ef13bcc4c
ArthurOliveira01 <arthurods37@gmail.com>
2023-02-13 14:51:46 -0400

    moving code

---
+++ src/Usuario.js
@@ -0,0 +1,11 @@
+export default function Usuario(){
+    <div class="usuario">
+    <img src="assets/catanacomics.svg" alt="imagem de perfil"/>
+    <div class="texto">
+      <span>
+        <strong>catanacomics</strong>
+        <ion-icon name="pencil"></ion-icon>
+      </span>
+    </div>
+  </div>
+}
